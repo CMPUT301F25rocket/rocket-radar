78a53e3d0daa280ccffe2d2b7d7017a28c2bf031
GenericConfluent <m13power@gmail.com>
2025-10-21 18:08:14 -0600

    chore: add material dependency

--- src/app/build.gradle.kts
+++ src/app/build.gradle.kts
@@ -32,11 +32,13 @@ android {
 }
 
 dependencies {
-
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.recyclerview)
+
+    // Instrumentation testing
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
