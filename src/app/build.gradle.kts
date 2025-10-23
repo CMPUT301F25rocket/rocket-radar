6849c5fe36a7aa06e7447c2b91b338a5f88dc77a
GenericConfluent <m13power@gmail.com>
2025-10-23 10:10:30 -0600

    chore: enable view and data binding

--- src/app/build.gradle.kts
+++ src/app/build.gradle.kts
@@ -6,6 +6,18 @@ android {
     namespace = "com.rocket.radar"
     compileSdk = 36
 
+    buildFeatures {
+        // This enables the generation of classes that basically contain all elements of a view by
+        // id so that findViewById only needs to be called rarely.
+        // Add `tools:viewBindingIgnore="true" to the root of layout files we don't want to bind.
+        // For more information see: https://developer.android.com/topic/libraries/view-binding
+        viewBinding = true
+        
+        // This enables automatic updating of views from our view model classes.
+        // For more information see: https://developer.android.com/topic/libraries/data-binding
+        dataBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.rocket.radar"
         minSdk = 24
