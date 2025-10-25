c7d0e2690b69cb4d6f3ee3a1ab1719a39730f7d0
ArtDynasty13 <arthurwitte18@gmail.com>
2025-10-25 11:48:02 -0600

    chore: firebase gradle dependencies

--- src/app/build.gradle.kts
+++ src/app/build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
+
+    id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -54,4 +57,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    implementation(platform("com.google.firebase:firebase-bom:34.4.0"))
+    implementation("com.google.firebase:firebase-analytics")
 }
