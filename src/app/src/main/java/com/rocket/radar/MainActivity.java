5266bff5dc527322b7f5279968b8bb9535830d20
ArtDynasty13 <arthurwitte18@gmail.com>
2025-10-25 12:18:29 -0600

    chore: add firebase boilerplate to MainActivity

--- src/app/src/main/java/com/rocket/radar/MainActivity.java
+++ src/app/src/main/java/com/rocket/radar/MainActivity.java
@@ -1,18 +1,33 @@
 package com.rocket.radar;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.rocket.radar.databinding.NavBarBinding;
 
 public class MainActivity extends AppCompatActivity {
     private NavBarBinding navBarBinding;
 
+    private static final String TAG = "MainActivity";
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mAuth = FirebaseAuth.getInstance(); // Initiaize Firebase Auth
+
         super.onCreate(savedInstanceState);
         navBarBinding = NavBarBinding.inflate(getLayoutInflater());
         setContentView(navBarBinding.getRoot());
@@ -37,4 +52,44 @@ public class MainActivity extends AppCompatActivity {
             return true;
         });
     }
+}
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser == null)
+            signInAnonymously();
+        else
+            updateUI(currentUser);
+    }
+
+    private void signInAnonymously() {
+        mAuth.signInAnonymously()
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInAnonymously:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInAnonymously:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+    }
+
+    private void updateUI(FirebaseUser user) {
+        if (user != null)
+            Log.d(TAG, "User ID: " + user.getUid()); // TODO: Load profile fragment or display user info
+        else
+            Log.d(TAG, "No user signed in"); // TODO: Show sign-in error message or retry
+    }
+
 }
