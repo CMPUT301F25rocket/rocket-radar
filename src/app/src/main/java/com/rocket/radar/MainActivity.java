0c17eaab85904119937cdae1b5879def2f20e491
Bernabe Kondrak <b.kondrak45@gmail.com>
2025-10-28 14:35:50 -0600

    navbar functionality

--- src/app/src/main/java/com/rocket/radar/MainActivity.java
+++ src/app/src/main/java/com/rocket/radar/MainActivity.java
@@ -2,7 +2,9 @@ package com.rocket.radar;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.rocket.radar.databinding.NavBarBinding;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,5 +17,24 @@ public class MainActivity extends AppCompatActivity {
         navBarBinding = NavBarBinding.inflate(getLayoutInflater());
         setContentView(navBarBinding.getRoot());
 
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, new EventListFragment())
+                .commit();
+        }
+
+        navBarBinding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+            if (item.getItemId() == R.id.action_events) {
+                selectedFragment = new EventListFragment();
+            }
+
+            if (selectedFragment != null) {
+                getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, selectedFragment)
+                    .commit();
+            }
+            return true;
+        });
     }
 }
