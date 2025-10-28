3edc5e898afc809c6e4e1e7a80578a59f4bbc45d
Braden Woods <bwoodsy12@gmail.com>
2025-10-28 11:40:20 -0600

    feat: working base setup for notification page

--- src/app/src/main/java/com/rocket/radar/NotificationAdapter.java
+++ src/app/src/main/java/com/rocket/radar/NotificationAdapter.java
@@ -1,5 +1,61 @@
 package com.rocket.radar;
 
-public class NotificationAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapter.MyViewHolder> {
+    Context context;
+    List<Notification> notificationList;
+
+    public NotificationAdapter(Context context, List<Notification> notificationList) {
+        this.context = context;
+        this.notificationList = notificationList;
+    }
+
+    @NonNull
+    @Override
+    public NotificationAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        //inflate layout
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.event_notification_item, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NotificationAdapter.MyViewHolder holder, int position) {
+        // bind data to the rows in recycler view based on position
+        holder.eventImage.setImageResource(notificationList.get(position).getImage());
+        holder.eventTitle.setText(notificationList.get(position).getEventTitle());
+        holder.notificationType.setText(notificationList.get(position).getNotificationType());
+    }
+
+    @Override
+    public int getItemCount() {
+        // recycler view wants to know the number of items in the recycler view
+        return notificationList.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        // grabs the view from our event_notification_item.xml file
+        ImageView eventImage;
+        TextView eventTitle, notificationType;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            eventImage = itemView.findViewById(R.id.event_background_image);
+            eventTitle = itemView.findViewById(R.id.event_title_text);
+            notificationType = itemView.findViewById(R.id.update_details_text);
+
+        }
+    }
 }
--- src/app/src/main/java/com/rocket/radar/NotificationFragment.java
+++ src/app/src/main/java/com/rocket/radar/NotificationFragment.java
@@ -7,6 +7,8 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -20,28 +22,39 @@ public class NotificationFragment extends Fragment {
     private TextView emptyNotificationsTextView;
 
     // Adapter and Data
-    //private NotificationAdapter adapter;
+    private NotificationAdapter adapter;
     private List<Notification> notificationList;
 
     public NotificationFragment() {
     }
 
-//    private void setupRecyclerView() {
-//        notificationList = new ArrayList<>();
-//        NotificationAdapter adapter = new NotificationAdapter(notificationList, this);
-//        notificationRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-//        notificationRecyclerView.setAdapter(adapter);
-//    }
+    private void setupRecyclerView() {
+        adapter = new NotificationAdapter(getContext(), notificationList);
+        notificationRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        notificationRecyclerView.setAdapter(adapter);
+    }
 
+    private void loadDummyData() {
+        Notification dummyNotif1 = new Notification("Campus Marathon", "Event Update", false, 0);
+        Notification dummyNotif2 = new Notification("Charity Gala", "Reminder", true, 0);
+        Notification dummyNotif3 = new Notification("Coding Competition", "Winner Announcement", false, 0);
+        Notification dummyNotif4 = new Notification("Blood Drive", "New Event", false, 0);
+        Notification dummyNotif5 = new Notification("Volunteer Meetup", "Feedback Request", true, 0);
+        notificationList.add(dummyNotif1);
+        notificationList.add(dummyNotif2);
+        notificationList.add(dummyNotif3);
+        notificationList.add(dummyNotif4);
+        notificationList.add(dummyNotif5);
+        adapter.notifyDataSetChanged();
+    }
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.notification_list, container, false);
 
         notificationRecyclerView = view.findViewById(R.id.notification_recycler_view);
         emptyNotificationsTextView = view.findViewById(R.id.empty_notifications_text);
         Button backButton = view.findViewById(R.id.back_arrow);
-
-        // setupRecyclerView();
 
         emptyNotificationsTextView.setVisibility(View.VISIBLE);
 
@@ -53,9 +66,17 @@ public class NotificationFragment extends Fragment {
         return view;
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        // Initialize UI components
+        super.onViewCreated(view, savedInstanceState);
 
+        notificationList = new ArrayList<>();
+        setupRecyclerView();
+        loadDummyData();
+    }
 
-
-
-
+    private void updateEmptyViewVisibility(){
+        emptyNotificationsTextView.setVisibility(View.GONE);
+    }
 }
