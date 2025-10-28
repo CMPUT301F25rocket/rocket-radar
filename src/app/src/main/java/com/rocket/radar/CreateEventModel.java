bf9de270ec8d1ffae27b64a5cc9ea687a2ce89ee
GenericConfluent <m13power@gmail.com>
2025-10-28 17:29:19 -0600

    feat(create event; #25 US 02.01.01): complete model definition and bind
    There are still some issues to resolve. Currently the implementation
    uses a `FragmentContainerView`, but it may be the case that another
    class can be used. `ViewSwitcher` doesn't work for the use case since it
    only allows switching between 2 child views.
    
    The next commit will be an attempt at using `FrameLayoutView` and swapping
    the input section manually which seems to be the way the implementation
    is headed. If that doesn't work it will changed back to
    `FragmentContainerView`.

--- src/app/src/main/java/com/rocket/radar/CreateEventModel.java
+++ src/app/src/main/java/com/rocket/radar/CreateEventModel.java
@@ -1,7 +1,16 @@
 package com.rocket.radar;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.os.Build;
+import android.view.View;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.Bindable;
+import androidx.databinding.Observable;
 import androidx.lifecycle.ViewModel;
 
+import java.time.ZonedDateTime;
 import java.util.Date;
 
 class Time {
@@ -10,7 +19,10 @@ class Time {
 }
 
 enum Section {
+    // WARN: The order here is important. Section transitions will happen in the order these appear in.
     GENERAL, DATETIME, DEADLINES, LOTTERY, STYLE;
+
+    public static final Section lastSection = Section.values()[Section.values().length - 1];
 
     // TODO: Pull these from strings.xml.
 
@@ -35,24 +47,105 @@ enum Section {
         }
         throw new IllegalArgumentException("Section variant " + this + " has no title defined.");
     }
+
+    public boolean isGeneralValid(String title, String description) {
+        return !title.isBlank() && !description.isBlank();
+    }
+
+    public boolean isDatetimeValid(Date eventDate, Time eventStartTime, Time eventEndTime) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // return ZonedDateTime.from(eventDate).isAfter(ZonedDateTime.now())
+        }
+        return true;
+    }
 }
 
-class CreateEventModel extends ViewModel {
+class CreateEventModel extends ViewModel implements Observable {
     // Creation wizard state.
     private Section section;
 
-    // General
+    // General section field values
     private String title;
     private String description;
 
 
+    // Datetime section field values
     private Boolean singleDayEvent;
     private Date eventDate;
     private Time eventStartTime;
     private Time eventEndTime;
 
+    // Deadline section field values
+    private Date registrationStartDate;
+    private Date registrationEndDate;
 
+    private Date initialSelectionStartDate;
+    private Date initialSelectionEndDate;
+
+    private Date finalAttendeeSelectionDate;
+
+    // Lottery section field values
+    private boolean hasWaitlistCapacity;
+    private boolean hasLocationRequirement;
+    private int waitlistCapacity;
+    private int eventCapacity;
+    private Date lotteryDate;
+    private Time lotteryTime;
+
+    // Style section field values
+    private Bitmap image;
+    private Color color;
+
+    public CreateEventModel() {
+        section = Section.GENERAL;
+    }
+
+    @Override
+    public void addOnPropertyChangedCallback(OnPropertyChangedCallback callback) {
+
+    }
+
+    @Override
+    public void removeOnPropertyChangedCallback(OnPropertyChangedCallback callback) {
+
+    }
+
+    @Bindable
     public Section getSection() {
         return section;
+    }
+
+    @Bindable
+    public int getLeftButtonVisibility() {
+        if (section != Section.GENERAL) {
+            return View.GONE;
+        } else {
+            return View.VISIBLE;
+        }
+    }
+
+    @Bindable
+    public int getRightButtonVisibility() {
+        return View.VISIBLE;
+    }
+
+    @Bindable
+    public String getRightButtonText() {
+        if (section == Section.STYLE) return "Create";
+        else return "Next";
+    }
+
+    public void rightButtonPress() {
+        if (section == Section.lastSection) {
+            // TODO: Create event
+            return;
+        } else {
+            // Set the next section sequentially.
+            section = Section.values()[section.ordinal() + 1];
+        }
+    }
+
+    public void backSection() {
+
     }
 }
