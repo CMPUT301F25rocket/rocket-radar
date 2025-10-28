be34301f90ddde4a77485fe73ffae51f91dc459f
GenericConfluent <m13power@gmail.com>
2025-10-28 17:43:20 -0600

    feat(create event; #25 US 02.01.01): make the fragments and inflate

---
+++ src/app/src/main/java/com/rocket/radar/DraftEventsFragment.java
@@ -0,0 +1,34 @@
+package com.rocket.radar;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.rocket.radar.databinding.FragmentOrganizingEventsBinding;
+
+public class DraftEventsFragment extends Fragment {
+    FragmentOrganizingEventsBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentOrganizingEventsBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        binding = null;
+    }
+}
