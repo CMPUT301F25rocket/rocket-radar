9a50f927c02b7ac3fd4c6b2bb76e4679b222cb59
Bernabe Kondrak <b.kondrak45@gmail.com>
2025-10-28 14:15:10 -0600

    fix: added strings resources from morning sesh
    also put navbar on mainactivity, still need to implement logic that
    takes us to fragment

--- src/app/src/main/java/com/rocket/radar/MainActivity.java
+++ src/app/src/main/java/com/rocket/radar/MainActivity.java
@@ -1,27 +1,19 @@
 package com.rocket.radar;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.rocket.radar.databinding.NavBarBinding;
 
 public class MainActivity extends AppCompatActivity {
     private NavBarBinding navBarBinding;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        navBarBinding = NavBarBinding.inflate(getLayoutInflater());
+        setContentView(navBarBinding.getRoot());
+
     }
 }
