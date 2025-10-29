24bba74a6946ea277ccb5b14e41ccc20cf050921
Braden Woods <bwoodsy12@gmail.com>
2025-10-28 19:34:26 -0600

    chore: bshtf

--- src/app/src/main/java/com/rocket/radar/EventViewFragment.java
+++ src/app/src/main/java/com/rocket/radar/EventViewFragment.java
@@ -1,5 +1,34 @@
 package com.rocket.radar;
 
+import static android.app.PendingIntent.getActivity;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
 public class EventViewFragment {
 
+    private final Event event;
+
+    public EventViewFragment(Event event) {
+        this.event = event;
+    }
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.event_view, container, false);
+
+        // TODO: Change toolbar to a button
+        Button backButton = view.findViewById(R.id.toolbar);
+//        backButton.setOnClickListener(v -> {
+//            if (getActivity() != null) {
+//                getActivity().getSupportFragmentManager().popBackStack();
+//            }
+//        });
+
+        return view;
+
+    }
 }
