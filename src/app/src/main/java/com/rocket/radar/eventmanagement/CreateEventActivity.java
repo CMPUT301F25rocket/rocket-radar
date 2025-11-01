1ebc131ad98ada90e2ae691cfe4ef4bc717434bb
GenericConfluent <m13power@gmail.com>
2025-10-31 21:11:22 -0600

    feat(US 02.01.01; #25): some styling and plan bottom sheet picker

--- src/app/src/main/java/com/rocket/radar/eventmanagement/CreateEventActivity.java
+++ src/app/src/main/java/com/rocket/radar/eventmanagement/CreateEventActivity.java
@@ -5,6 +5,8 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.android.material.textfield.TextInputEditText;
+import com.rocket.radar.R;
 import com.rocket.radar.databinding.ActivityCreateEventBinding;
 
 /**
@@ -32,7 +34,7 @@ public class CreateEventActivity extends AppCompatActivity {
         // >:? This needs to be set in code.
         bottomSheetBehavior = BottomSheetBehavior.from(binding.createEventWizardBottomSheet);
 
-        // FIXME: Need to figure out how to notify view that section was changed.
+        // Main navigation buttons
         binding.createEventWizardNavLeftButton.setOnClickListener(btn -> {
             model.prevSection();
         });
@@ -40,6 +42,7 @@ public class CreateEventActivity extends AppCompatActivity {
             model.nextSection();
         });
 
+        // Unfortunately it seems viewbinding fails for included views.
         setContentView(binding.getRoot());
         binding.setLifecycleOwner(this);
     }
@@ -48,6 +51,35 @@ public class CreateEventActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
         bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+
+        // Now we need to bind the inputs that function by sheet.
+        TextInputEditText pickEventDate = binding.getRoot().findViewById(R.id.inputEventDatetimeDaterangeTextInput);
+        TextInputEditText pickEventStart = binding.getRoot().findViewById(R.id.inputEventDatetimeStartTextInput);
+        TextInputEditText pickEventEnd = binding.getRoot().findViewById(R.id.inputEventDatetimeEndTextInput);
+
+        pickEventDate.setOnClickListener(itxt -> {
+
+        });
+
+        pickEventStart.setOnClickListener(itxt -> {
+            // TODO: Tommorow.
+            // Ideally need to be able to specify the destination to save the picked value in.
+            // Probably want a callback that is run whenever the datetime date is selected, time
+            // is set, or color is chosen. Something like DateSheetPicker.onSelect(value -> model.eventDate = value);
+            // That means I need dedicated classes for each picker type I want to implement so thats
+            // going to be a pain.
+
+            // SheetPicker<T>. Thats a better abstraction. Set the type T, and provide an instance
+            // which implements a picker interface that can inflate the relevant picker into the
+            // bottom sheet. Accept a callback to set value T when a selection happens. etc.
+            // I probably want a single sheetpicker per xml view that has a bottom sheet which luckily
+            // for this activity is just one. Then the SheetPicker<T> can own the BottomSheetBehaviour
+            // And deal with the annoying stuff for me.
+        });
+
+        pickEventEnd.setOnClickListener(itxt -> {
+
+        });
     }
 
     @Override
