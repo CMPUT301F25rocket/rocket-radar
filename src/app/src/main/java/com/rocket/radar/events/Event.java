9eb85fc7e7a838d9135cc21037bd21653017b6f7
Bernabe Kondrak <b.kondrak45@gmail.com>
2025-11-07 13:10:38 -0700

    chore: javadoc for Event.java

--- src/app/src/main/java/com/rocket/radar/events/Event.java
+++ src/app/src/main/java/com/rocket/radar/events/Event.java
@@ -3,6 +3,7 @@ package com.rocket.radar.events;
 import java.io.ByteArrayOutputStream;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.time.format.TextStyle;
 import java.util.List;
@@ -25,11 +26,12 @@ import java.io.Serializable;
 /**
  * Represents a single event in the application.
  *
- * This class serves as a data model for event information, including its title, date,
- * and a unique identifier. It is designed to be serialized for passing between
+ * <p>This class serves as a data model for event information, including details about the event,
+ * scheduling, registration, and user participation. It is designed to be serialized for passing between
  * Android components (e.g., Fragments) and is compatible with Google Firestore for
  * database operations. The {@link Serializable} interface allows Event objects to be
- * passed in Bundles.
+ * passed in Bundles.</p>
+ * <p>A {@link Builder} class is provided for flexible object creation.</p>
  */
 public class Event implements Serializable {
     // This is the fixed list of categories that
@@ -74,19 +76,46 @@ public class Event implements Serializable {
     private transient Bitmap bannerImage;
     private Color color;
 
-    // Join Management
+    /**
+     * A list of user IDs for those on the waitlist for the event.
+     * These users may be moved to the attending list if spots become available.
+     */
     ArrayList<String> eventWaitlistIds;
+    /**
+     * A list of user IDs for those who have been invited to the event.
+     * This is typically used in specific event types where invitations are sent out.
+     */
     ArrayList<String> eventInvitedIds;
+    /**
+     * A list of user IDs for those who are confirmed to be attending the event.
+     */
     ArrayList<String> eventAttendingIds;
+    /**
+     * A list of user IDs for those who have cancelled their attendance or registration.
+     */
     ArrayList<String> eventCancelledIds;
 
     int image;
 
+    /**
+     * Default constructor required for Firestore data mapping.
+     * Initializes transient fields to null.
+     */
     public Event() {
         // Default constructor required
         bannerImage = null;
     }
 
+    /**
+     * Constructs a new Event with essential details.
+     * A unique event ID is automatically generated.
+     *
+     * @param eventTitle The title of the event.
+     * @param date The start date of the event.
+     * @param tagline A short, catchy phrase for the event.
+     * @param description A detailed description of the event.
+     * @param image A local drawable resource ID for a placeholder image.
+     */
     public Event(String eventTitle, Date date, String tagline, String description, int image) {
         this.eventId = UUID.randomUUID().toString(); // Generate a unique ID
         this.eventTitle = eventTitle;
@@ -107,6 +136,11 @@ public class Event implements Serializable {
      * Gets the date of the event.
      * @return The event date as a String.
      */
+    /**
+     * Gets the start date of the event.
+     *
+     * @return The {@link Date} object representing the event's start date, or null if not set.
+     */
     public Date getEventStartDate() { return eventStartDate != null ? eventStartDate : null;}
     public String getFormattedDate() {
         // returns the date in format DD\nMMM where MMM three letter capital abbreviation for the month
@@ -117,6 +151,12 @@ public class Event implements Serializable {
         String month = localDate.getMonth().getDisplayName(TextStyle.SHORT, Locale.ENGLISH).toUpperCase();
         return day + "\n" + month;
     }
+
+    /**
+     * Gets the tagline of the event.
+     *
+     * @return The event tagline as a String.
+     */
     public String getTagline() { return tagline; }
     /**
      * Gets the unique identifier of the event.
@@ -132,134 +172,299 @@ public class Event implements Serializable {
     public void setEventId(String eventId) { this.eventId = eventId; }
 
 
+    /**
+     * Gets the associated color for the event as an ARGB integer.
+     *
+     * @return The integer representation of the event's color.
+     */
     public int getColor() {
         return color.toArgb();
     }
 
+    /**
+     * Sets the color for the event from an ARGB integer.
+     *
+     * @param color The integer representation of the color.
+     */
     public void setColor(int color) {
         this.color = Color.valueOf(color);
     }
 
+    /**
+     * Gets the time of the lottery draw.
+     *
+     * @return The {@link Time} of the lottery.
+     */
     public Time getLotteryTime() {
         return lotteryTime;
     }
 
+    /**
+     * Sets the time of the lottery draw.
+     *
+     * @param lotteryTime The {@link Time} of the lottery.
+     */
     public void setLotteryTime(Time lotteryTime) {
         this.lotteryTime = lotteryTime;
     }
 
+    /**
+     * Gets the date of the lottery draw.
+     *
+     * @return The {@link Date} of the lottery.
+     */
     public Date getLotteryDate() {
         return lotteryDate;
     }
 
+    /**
+     * Sets the date of the lottery draw.
+     *
+     * @param lotteryDate The {@link Date} of the lottery.
+     */
     public void setLotteryDate(Date lotteryDate) {
         this.lotteryDate = lotteryDate;
     }
 
+    /**
+     * Gets the maximum capacity of the event.
+     *
+     * @return The event's capacity.
+     */
     public int getEventCapacity() {
         return eventCapacity;
     }
 
+    /**
+     * Sets the maximum capacity of the event.
+     *
+     * @param eventCapacity The event's capacity.
+     */
     public void setEventCapacity(int eventCapacity) {
         this.eventCapacity = eventCapacity;
     }
 
+    /**
+     * Checks if location tracking is required for this event.
+     *
+     * @return true if location is required, false otherwise.
+     */
     public boolean isRequireLocation() {
         return requireLocation;
     }
 
+    /**
+     * Sets whether location tracking is required for this event.
+     *
+     * @param requireLocation true to require location, false otherwise.
+     */
     public void setRequireLocation(boolean requireLocation) {
         this.requireLocation = requireLocation;
     }
 
+    /**
+     * Gets the capacity of the waitlist.
+     *
+     * @return An {@link Integer} representing the waitlist capacity, or null if not set.
+     */
     public Integer getWaitlistCapacity() {
         return waitlistCapacity.orElse(null);
     }
 
+    /**
+     * Sets the capacity of the waitlist.
+     *
+     * @param waitlistCapacity The waitlist capacity. Can be null.
+     */
     public void setWaitlistCapacity(Integer waitlistCapacity) {
         this.waitlistCapacity = Optional.ofNullable(waitlistCapacity);
     }
 
+    /**
+     * Gets the final date for selections to be made.
+     *
+     * @return The final selection {@link Date}.
+     */
     public Date getFinalSelectionDate() {
         return finalSelectionDate;
     }
 
+    /**
+     * Sets the final date for selections.
+     *
+     * @param finalSelectionDate The final selection {@link Date}.
+     */
     public void setFinalSelectionDate(Date finalSelectionDate) {
         this.finalSelectionDate = finalSelectionDate;
     }
 
+    /**
+     * Gets the end date of the selection period.
+     *
+     * @return The selection end {@link Date}.
+     */
     public Date getSelectionEndDate() {
         return selectionEndDate;
     }
 
+    /**
+     * Sets the end date of the selection period.
+     *
+     * @param selectionEndDate The selection end {@link Date}.
+     */
     public void setSelectionEndDate(Date selectionEndDate) {
         this.selectionEndDate = selectionEndDate;
     }
 
+    /**
+     * Gets the start date of the selection period.
+     *
+     * @return The selection start {@link Date}.
+     */
     public Date getSelectionStartDate() {
         return selectionStartDate;
     }
 
+    /**
+     * Sets the start date of the selection period.
+     *
+     * @param selectionStartDate The selection start {@link Date}.
+     */
     public void setSelectionStartDate(Date selectionStartDate) {
         this.selectionStartDate = selectionStartDate;
     }
 
+    /**
+     * Gets the end date of the registration period.
+     *
+     * @return The registration end {@link Date}.
+     */
     public Date getRegistrationEndDate() {
         return registrationEndDate;
     }
 
+    /**
+     * Sets the end date of the registration period.
+     *
+     * @param registrationEndDate The registration end {@link Date}.
+     */
     public void setRegistrationEndDate(Date registrationEndDate) {
         this.registrationEndDate = registrationEndDate;
     }
 
+    /**
+     * Gets the start date of the registration period.
+     *
+     * @return The registration start {@link Date}.
+     */
     public Date getRegistrationStartDate() {
         return registrationStartDate;
     }
 
+    /**
+     * Sets the start date of the registration period.
+     *
+     * @param registrationStartDate The registration start {@link Date}.
+     */
     public void setRegistrationStartDate(Date registrationStartDate) {
         this.registrationStartDate = registrationStartDate;
     }
 
+    /**
+     * Gets the end time of the event.
+     *
+     * @return The event end {@link Time}.
+     */
     public Time getEventEndTime() {
         return eventEndTime;
     }
 
+    /**
+     * Sets the end time of the event.
+     *
+     * @param eventEndTime The event end {@link Time}.
+     */
     public void setEventEndTime(Time eventEndTime) {
         this.eventEndTime = eventEndTime;
     }
 
+    /**
+     * Gets the start time of the event.
+     *
+     * @return The event start {@link Time}.
+     */
     public Time getEventStartTime() {
         return eventStartTime;
     }
 
+    /**
+     * Sets the start time of the event.
+     *
+     * @param eventStartTime The event start {@link Time}.
+     */
     public void setEventStartTime(Time eventStartTime) {
         this.eventStartTime = eventStartTime;
     }
 
+    /**
+     * Gets the end date of the event.
+     *
+     * @return The event end {@link Date}, or null if not set.
+     */
     public Date getEventEndDate() {
         return eventEndDate.orElse(null);
     }
 
+    /**
+     * Sets the end date of the event.
+     *
+     * @param eventEndDate The event end {@link Date}. Can be null.
+     */
     public void setEventEndDate(Date eventEndDate) {
         this.eventEndDate = Optional.ofNullable(eventEndDate);
     }
 
+    /**
+     * Sets the start date of the event.
+     *
+     * @param eventStartDate The event start {@link Date}.
+     */
     public void setEventStartDate(Date eventStartDate) {
         this.eventStartDate = eventStartDate;
     }
 
+    /**
+     * Gets the categories associated with the event.
+     *
+     * @return A {@link List} of category strings.
+     */
     public List<String> getCategories() {
         return new ArrayList<>(categories);
     }
 
+    /**
+     * Sets the categories for the event.
+     *
+     * @param categories A {@link List} of category strings.
+     */
     public void setCategories(List<String> categories) {
         this.categories = new TreeSet<>(categories);
     }
 
+    /**
+     * Sets the tagline for the event.
+     *
+     * @param tagline The event tagline.
+     */
     public void setTagline(String tagline) {
         this.tagline = tagline;
     }
 
+    /**
+     * Sets the title of the event.
+     *
+     * @param eventTitle The event title.
+     */
     public void setEventTitle(String eventTitle) {
         this.eventTitle = eventTitle;
     }
@@ -273,10 +478,20 @@ public class Event implements Serializable {
     @com.google.firebase.firestore.Exclude
     public int getImage() { return image; }
 
+    /**
+     * Gets the detailed description of the event.
+     *
+     * @return The event description.
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Sets the detailed description of the event.
+     *
+     * @param description The event description.
+     */
     public void setDescription(String description) {
         this.description = description;
     }
@@ -295,7 +510,18 @@ public class Event implements Serializable {
         return bannerImage;
     }
 
+    /**
+     * Gets the banner image as a Firestore {@link Blob}.
+     * This is the format used for database storage.
+     *
+     * @return The banner image as a Blob.
+     */
     public Blob getBannerImageBlob() { return this.bannerImageBlob; }
+    /**
+     * Sets the banner image from a Firestore {@link Blob}.
+     *
+     * @param data The banner image data as a Blob.
+     */
     public void setBannerImageBlob(Blob data) { this.bannerImageBlob = data; }
 
     private static Bitmap resizeBanner(Bitmap image)
@@ -353,14 +579,32 @@ public class Event implements Serializable {
         return cropped;
     }
 
+    /**
+     * Gets the list of user IDs who have cancelled their registration.
+     *
+     * @return An {@link ArrayList} of user ID strings. Returns an empty list if null.
+     */
     public ArrayList<String> getEventCancelledIds() {
+        if (eventCancelledIds == null) {
+            eventCancelledIds = new ArrayList<>();
+        }
         return eventCancelledIds;
     }
 
+    /**
+     * Sets the list of user IDs who have cancelled their registration.
+     *
+     * @param eventCancelledIds An {@link ArrayList} of user ID strings.
+     */
     public void setEventCancelledIds(ArrayList<String> eventCancelledIds) {
         this.eventCancelledIds = eventCancelledIds;
     }
 
+    /**
+     * Gets the list of user IDs attending the event.
+     *
+     * @return An {@link ArrayList} of user ID strings. Returns an empty list if null.
+     */
     public ArrayList<String> getEventAttendingIds() {
         if (eventAttendingIds == null) {
             eventAttendingIds = new ArrayList<>();
@@ -368,10 +612,20 @@ public class Event implements Serializable {
         return eventAttendingIds;
     }
 
+    /**
+     * Sets the list of user IDs attending the event.
+     *
+     * @param eventAttendingIds An {@link ArrayList} of user ID strings.
+     */
     public void setEventAttendingIds(ArrayList<String> eventAttendingIds) {
         this.eventAttendingIds = eventAttendingIds;
     }
 
+    /**
+     * Gets the list of user IDs invited to the event.
+     *
+     * @return An {@link ArrayList} of user ID strings. Returns an empty list if null.
+     */
     public ArrayList<String> getEventInvitedIds() {
         if (eventInvitedIds == null) {
             eventInvitedIds = new ArrayList<>();
@@ -379,10 +633,20 @@ public class Event implements Serializable {
         return eventInvitedIds;
     }
 
+    /**
+     * Sets the list of user IDs invited to the event.
+     *
+     * @param eventInvitedIds An {@link ArrayList} of user ID strings.
+     */
     public void setEventInvitedIds(ArrayList<String> eventInvitedIds) {
         this.eventInvitedIds = eventInvitedIds;
     }
 
+    /**
+     * Gets the list of user IDs on the event's waitlist.
+     *
+     * @return An {@link ArrayList} of user ID strings. Returns an empty list if null.
+     */
     public ArrayList<String> getEventWaitlistIds() {
         if (eventWaitlistIds == null) {
             eventWaitlistIds = new ArrayList<>();
@@ -390,6 +654,11 @@ public class Event implements Serializable {
         return eventWaitlistIds;
     }
 
+    /**
+     * Sets the list of user IDs on the event's waitlist.
+     *
+     * @param eventWaitlistIds An {@link ArrayList} of user ID strings.
+     */
     public void setEventWaitlistIds(ArrayList<String> eventWaitlistIds) {
         this.eventWaitlistIds = eventWaitlistIds;
     }
