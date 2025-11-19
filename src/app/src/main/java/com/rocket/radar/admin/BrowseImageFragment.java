8ea04a2165102e5a0e71547e2b55da2c2a97817a
GenericConfluent <m13power@gmail.com>
2025-11-19 11:58:39 -0700

    feat(admin; US 03.06.01 #52): WIP scaffold for image browsing

---
+++ src/app/src/main/java/com/rocket/radar/admin/BrowseImageFragment.java
@@ -0,0 +1,33 @@
+package com.rocket.radar.admin;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+
+import com.rocket.radar.databinding.FragmentBrowseImagesBinding;
+
+import java.util.ArrayList;
+
+public class BrowseImageFragment extends Fragment {
+    private FragmentBrowseImagesBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentBrowseImagesBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        binding.fullImageList.setLayoutManager(new GridLayoutManager(getContext(), 3));
+        binding.fullImageList.setAdapter(new ImageAdapter(new ArrayList<>()));
+
+    }
+}
---
+++ src/app/src/main/java/com/rocket/radar/admin/ImageAdapter.java
@@ -0,0 +1,52 @@
+package com.rocket.radar.admin;
+
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ViewHolder> {
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private final ImageView imageView;
+
+        public ViewHolder(ImageView view) {
+            super(view);
+            imageView = view;
+        }
+
+        public ImageView getImageView() {
+            return imageView;
+        }
+    }
+
+    List<Bitmap> images;
+
+    public ImageAdapter(List<Bitmap> images) {
+        this.images = images;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // https://stackoverflow.com/questions/6180802/show-imageview-programmatically
+        ImageView imageView = new ImageView(parent.getContext());
+        return new ViewHolder(imageView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        ImageView imageView = holder.getImageView();
+        imageView.setImageBitmap(images.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return images.size();
+    }
+}
